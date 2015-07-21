4f6c81452c97b81c4b78d1035ba950cd7c8a5f5e
James Houghton <James.p.houghton@gmail.com>
2015-07-21 16:41:23 -0400

    20150721

--- source/analyses/realtime/_twitter_credentials.py
+++
@@ -1,4 +0,0 @@
-consumer_key = '55inoHDaQsrNgyEOHETZVghef'
-consumer_secret = 'M0qwriroGJDwHwZ0wupW4ryARHZaueeqzJBTqs5U622LjQL1SK'
-access_token = '872256223-aAO7n2DJ51afV0OyqXeb7VkpY60CogxfUi0mzTBZ'
-access_token_secret = 'WOAXwNEuXGe6cL2jAHBjGSzp7iMkKEv9lIEsQfG1bgONa'
