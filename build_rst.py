8a35eebaaf1ec7fddce7140180c618722f55303f
Eneko Martin-Martinez <eneko.martin.martinez@gmail.com>
2022-09-13 22:11:57 +0200

    Solve bugs with non-converted notebooks

--- build_rst.py
+++ build_rst.py
@@ -1,42 +1,44 @@
 import glob
-import os.path
+from pathlib import Path
 import subprocess
 import shutil
+
+_root = Path(__file__).parent.resolve()
+_docs = _root / "docs"
 
 notebooks = glob.glob('source/analyses/*/*.ipynb')
 notebooks += glob.glob('source/data/*/*.ipynb')
 
 # Remove 'workbook' files
-notebooks = [nbf for nbf in notebooks if '_Workbook' not in nbf]
+notebooks = [Path(nbf) for nbf in notebooks if '_Workbook' not in nbf]
 errors = []
 
 for infile in notebooks:
-    try:
-        # convert notebook in its local directory
-        path, nbfile = os.path.split(infile)
-        call_str = "jupyter nbconvert --to rst %(nbfile)s" % {'nbfile': nbfile}
-        subprocess.call(call_str, cwd=path, shell=True)
+    # convert notebook in its local directory
+    path = _root / infile.parent
+    nbfile = infile.name
+    rstfile = infile.with_suffix(".rst").name
+    files = infile.with_suffix("").name + '_files'
+    call_str = "jupyter nbconvert --to rst %(nbfile)s" % {'nbfile': nbfile}
+    subprocess.call(call_str, cwd=path, shell=True)
 
-        # move the .rst file
-        no_ext = os.path.splitext(nbfile)[0]
-        movefiles = path + '/' + no_ext + '.rst'
-        # pretty dependent on directory structure
-        dest = 'docs/' + '/'.join(path.split('/')[1:])
-        # ! mv $movefiles $dest
-        shutil.move(movefiles, dest + '/' + no_ext + '.rst')
+    # move the .rst file
+    movefiles = path / rstfile
+    # pretty dependent on directory structure
+    dest = _docs / Path(*infile.parent.parts[1:])
+    # ! mv $movefiles $dest
 
-        # move supporting files, if there are any
-        movedir = path + '/' + no_ext + '_files'
-        if os.path.exists(movedir):
-            destdir = 'docs/' + '/'.join(path.split('/')[1:])\
-                + '/' + no_ext + '_files'
-            if os.path.exists(destdir):
-                # !rm - r $destdir
-                shutil.rmtree(destdir)
-            # !mv $movedir $dest
-            shutil.move(movedir, dest)
-    except (FileNotFoundError, NotADirectoryError):
-        errors.append(f"'{infile}'")
+    if not dest.exists():
+        dest.mkdir()
 
-print(f'Did not translate {", ".join(errors)}')
+    shutil.move(movefiles, dest / rstfile)
 
+    # move supporting files, if there are any
+    movedir = path / files
+    if movedir.exists():
+        destdir = dest / files
+        if destdir.exists():
+            # !rm - r $destdir
+            shutil.rmtree(destdir)
+        # !mv $movedir $dest
+        shutil.move(str(movedir), str(dest))
